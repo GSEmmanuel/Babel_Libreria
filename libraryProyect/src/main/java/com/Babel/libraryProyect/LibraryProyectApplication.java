bdf39c739d324bdbd21784bb1373cc65526d9ab9
mac <emmanuelgsan@gmail.com>
2021-08-01 01:00:10 -0500

    .

---
+++ libraryProyect/src/main/java/com/Babel/libraryProyect/LibraryProyectApplication.java
@@ -0,0 +1,13 @@
+package com.Babel.libraryProyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryProyectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryProyectApplication.class, args);
+	}
+
+}
---
+++ libraryProyect/src/test/java/com/Babel/libraryProyect/LibraryProyectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Babel.libraryProyect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryProyectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
